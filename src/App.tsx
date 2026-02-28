6e9a6f3c9bd6347043f4eb183c8dad6d365a3571
kolkoa <kolkoabe@gmail.com>
2026-02-28 14:57:11 -0500

    initial react + tailwind commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-blue-200">
+      <h1 className="text-4xl font-bold text-white">Tailwind is working!</h1>
+    </div>
+  )
+}
